5d20fe49d1f4e014d2b3f1dbfec313fe5ec2fbca
Tae Young Ryu <yty0819@gmail.com>
2022-04-08 22:07:53 +0900

    Resolve #10 - Add test

---
+++ src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParserTest();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+
+}
