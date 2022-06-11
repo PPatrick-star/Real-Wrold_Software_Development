9dcee954339b2831b92de4e11941368b56a4ec90
Tae Young Ryu <yty0819@gmail.com>
2022-06-11 17:35:30 +0900

    Resolve #46 - Modify letter importer

--- src/main/java/com/study/java/rwsd/chapter_4/LetterImporter.java
+++ src/main/java/com/study/java/rwsd/chapter_4/LetterImporter.java
@@ -1,11 +1,27 @@
 package com.study.java.rwsd.chapter_4;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import static com.study.java.rwsd.chapter_4.Attributes.*;
 
 class LetterImporter implements Importer{
+    private static final String NAME_PREFIX = "Dear ";
     @Override
-    public Document importFile(final File file) {
+    public Document importFile(final File file) throws IOException {
+        final TextFile textFile = new TextFile(file);
 
-        return new Document(null);
+        textFile.addLineSuffix(NAME_PREFIX, PATIENT);
+
+        final int lineNumber = textFile.addLines(2, String::isEmpty, ADDRESS);
+        textFile.addLines(lineNumber + 1, (line) -> line.startsWith("regards,"), BODY);
+
+        final Map<String, String> attributes = textFile.getAttributes();
+        attributes.put(TYPE, "LETTER");
+
+        return new Document(attributes);
     }
 }
